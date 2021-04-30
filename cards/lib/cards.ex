1568b4cf00ad2e0fe0d48458da7e3f30e5bb94cc
Gabriela Pereira <gabrielaprg17@gmail.com>
2021-04-30 14:06:53 -0300

    Add cards file

---
+++ cards/lib/cards.ex
@@ -0,0 +1,65 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @spec create_deck :: list
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    :erlang.binary_to_term binary
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason}-> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+
+  end
+end
