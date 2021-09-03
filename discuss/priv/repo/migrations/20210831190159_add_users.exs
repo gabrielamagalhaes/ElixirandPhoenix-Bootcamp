f68fa9377a3ab875dab994949f8284bd7d96be8c
Gabriela Pereira <gabrielaprg17@gmail.com>
2021-09-03 14:32:43 -0300

    Edits change method

--- discuss/priv/repo/migrations/20210831190159_add_users.exs
+++ discuss/priv/repo/migrations/20210831190159_add_users.exs
@@ -2,6 +2,13 @@ defmodule Discuss.Repo.Migrations.AddUsers do
   use Ecto.Migration
 
   def change do
+    create table("users") do
+      add :email, :string
+      add :provider, :string
+      add :token, :string
 
+      # mostra a hora de cada cadastro
+    timestamps()
+    end
   end
 end
