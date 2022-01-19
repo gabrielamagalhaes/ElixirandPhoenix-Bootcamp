ed56fc07f0830537f04d276203977040a93ce984
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-01-19 11:16:56 -0300

    updates auth_controller

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,8 +2,11 @@ defmodule Discuss.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
     # este metódo irá lidar com todas as infos que vierem do github
-   
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
   end
 end
