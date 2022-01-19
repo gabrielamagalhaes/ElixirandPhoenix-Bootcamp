7c44a94b9a159a0b590374f44690fa8d358f8876
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-01-19 11:17:36 -0300

    adds some commits on the code

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,7 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Repo
 
-# o controller receberá todas as requisições do usuário e irá se comunicar com a model
+  # o controller receberá todas as requisições do usuário e irá se comunicar com a model
 
   alias Discuss.Topic
 
@@ -10,38 +10,38 @@ defmodule DiscussWeb.TopicController do
     # lista todos os tópicos armazenados no bd
     topics = Repo.all(Topic)
     # renderiza com i idex ou índice onde mostrará uma lista com ourtos recursos
-    render conn, "index.html", topics: topics
+    render(conn, "index.html", topics: topics)
   end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     # cria a conexão com o arquivo html
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
     # mudanca de títulos no banco de dados
     changeset = Topic.changeset(%Topic{}, topic)
-
     # registro no banco de dados
     case Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
-        # muda de pag ao inserir um novo tópico/título
+
+      # muda de pag ao inserir um novo tópico/título
       {:error, changeset} ->
-        render conn, "new.html", changeset: changeset
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
   def edit(conn, %{"id" => topic_id}) do
     # encontrara o registro id de cada tópico
     topic = Repo.get(Topic, topic_id)
-    changeset =  Topic.changeset(topic)
+    changeset = Topic.changeset(topic)
 
-    render conn, "edit.html", changeset: changeset, topic: topic
+    render(conn, "edit.html", changeset: changeset, topic: topic)
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
@@ -54,17 +54,18 @@ defmodule DiscussWeb.TopicController do
       {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Updated")
-        # |> redirect(to: topic_path(conn, :index))
-        #  o redirect vai redicirecionar para a pag inicial, após a atualização
 
-        {:error, changeset} ->
-          render conn, "edit.html", changeset: changeset, topic: old_topic
+      # |> redirect(to: topic_path(conn, :index))
+      #  o redirect vai redicirecionar para a pag inicial, após a atualização
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
   end
 
-
   def delete(conn, %{"id" => topic_id}) do
-    Repo.get!(Topic, topic_id) |> Repo.delete!
+    Repo.get!(Topic, topic_id) |> Repo.delete!()
+
     # o exclamação significa que irá mostrar um erro na tela, por ex. no caso do get, ele vai pegar um id
     # mas caso o id não exista, ele mostrara esse erro, enfatizando que id solicitado não foi encontrado
 
--- discuss/lib/discuss_web/models/topic.ex
+++ discuss/lib/discuss_web/models/topic.ex
@@ -3,11 +3,10 @@ defmodule Discuss.Topic do
 
   #  model é a camada resposnável por relacionar o projeto com o banco de dados
 
-
   schema "topics" do
     field :title, :string
     belongs_to :user, Discuss.User
-    has_many :comments, Discuss.Comment
+    # has_many :comments, Discuss.Comment
   end
 
   # este método é o responsável por fazer a "chamada"
@@ -16,4 +15,9 @@ defmodule Discuss.Topic do
     |> cast(params, [:title])
     |> validate_required([:title])
   end
+
+  # struct representa um registro no banco de dados (o que é-atual)
+  # params representa como queremos atualizar o nosso dado(o que vai se tornar)
+  # cast registra a mudança (diz o que é(struct) e o que precisa ser(params))
+  # validate
 end
