20660348a4e174ca7d6e2dddcde9a3509112b15c
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-02-10 09:25:22 -0300

    adds index tests

--- discuss/test/discuss_web/controllers/topic_controller_test.exs
+++ discuss/test/discuss_web/controllers/topic_controller_test.exs
@@ -2,26 +2,24 @@ defmodule DiscussWeb.TopicControllerTest do
   use DiscussWeb.ConnCase
 
   import Discuss.Factory
-
-  # @listas [a, b, c, d]
+  alias Discuss.Repo
 
   describe "index/2 is success" do
-    test "index verifica tópico existente", %{conn: conn} do
-      # params = build(:topic_params)
-
+    test "check existing topic", %{conn: conn} do
       conn = get(conn, Routes.topic_path(conn, :index))
-
 
       assert html_response(conn, 200) =~ "Discuss · Phoenix Framework"
     end
 
-    # test "index lista todos os tópicos" do
-      # params =
+    test "check all existing topics", %{conn: conn} do
+      topic1 = insert(:topic)
+      topic2 = insert(:topic, title: "abcdef")
 
-      # conn = get(conn, Routes.topic_path(conn, :index))
+      response = get conn, Routes.topic_path(conn, :index)
 
-      # assert
-    # end
+
+      assert html_response(response, 200) =~ topic1.title
+      assert html_response(response, 200) =~ topic2.title
+    end
   end
-
 end
