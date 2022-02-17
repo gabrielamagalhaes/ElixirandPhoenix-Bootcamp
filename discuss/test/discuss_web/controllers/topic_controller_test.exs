d27a2c956cbd9c69bb91d975f800cfdf3ad81c44
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-02-17 13:56:23 -0300

    Adds create, edit and update test scenarios and adds a private method

--- discuss/test/discuss_web/controllers/topic_controller_test.exs
+++ discuss/test/discuss_web/controllers/topic_controller_test.exs
@@ -4,6 +4,9 @@ defmodule DiscussWeb.TopicControllerTest do
   import Discuss.Factory
   alias Discuss.Repo
 
+  @create_attrs %{title: "testando tópico" }
+  @update_attrs %{title: "testando tópico atualizado"}
+
   describe "index/2 is success" do
     test "check existing topic", %{conn: conn} do
       conn = get(conn, Routes.topic_path(conn, :index))
@@ -21,5 +24,46 @@ defmodule DiscussWeb.TopicControllerTest do
       assert html_response(response, 200) =~ topic1.title
       assert html_response(response, 200) =~ topic2.title
     end
+  end
+
+  describe "create/2 is success" do
+    test "create returns 200", %{conn: conn} do
+      conn = post(conn, Routes.topic_path(conn, :create), topic: @create_attrs)
+
+      assert redirected_to(conn) == Routes.topic_path(conn, :index)
+
+      conn = get(conn, Routes.topic_path(conn, :index))
+      assert html_response(conn, 200) =~ "Discuss · Phoenix Framework"
+    end
+  end
+
+  describe "edit/2 is success" do
+    setup [:create_topic]
+
+    test "edit returns 200", %{conn: conn, topic: topic} do
+      topic = insert(:topic)
+
+      conn = get(conn, Routes.topic_path(conn, :edit, topic))
+      assert html_response(conn, 200) =~ "Discuss · Phoenix Framework"
+    end
+  end
+
+  describe "update/3 is success" do
+    setup [:create_topic]
+
+    test "update returns 200", %{conn: conn, topic: topic} do
+      topic = insert(:topic)
+      conn = get(conn, Routes.topic_path(conn, :edit, topic), topic: @update_attrs)
+
+      assert redirected_to(conn) == Routes.topic_path(conn, :index)
+
+      conn = get(conn, Routes.topic_path(conn, :index))
+      assert html_response(conn, 200) =~ "The topic was updated"
+    end
+  end
+
+  defp create_topic(_) do
+    topic = topic_factory()
+    %{topic: topic}
   end
 end
