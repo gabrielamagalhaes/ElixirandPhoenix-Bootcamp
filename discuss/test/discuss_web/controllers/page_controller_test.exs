f07281f11e8bac58d9e682d981c45f29f5d99fa2
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-02-23 21:04:25 -0300

    fix page controller test

--- discuss/test/discuss_web/controllers/page_controller_test.exs
+++ discuss/test/discuss_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DiscussWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Discuss · Phoenix Framework"
   end
 end
